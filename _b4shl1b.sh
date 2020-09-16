12c82a1ea3e55584fff5160cc66f1884c848a11c
M°ŋα Ł¹ſα <spam@yourmom.com>
2020-09-16 22:45:06 +0200

    2020-09-16-20.45

--- _b4shl1b.sh
+++ _b4shl1b.sh
@@ -182,7 +182,7 @@ _git_autocommit() { echo -n;
 
 
 _html_userinfo() {
-    echo '<div id="userinfo">' ;
+    echo '<div id="userinfo" class="userinfo">' ;
     echo '<table id="userinfotable" class="userinfo"><tr>';
     for param in SCRIPT_NAME SSL_PROTOCOL SSL_CIPHER_USEKEYSIZE SSL_CIPHER_ALGKEYSIZE HTTP_USER_AGENT GATEWAY_INTERFACE ; do echo '<th>'${param}' </th>' ; done
     echo '</tr><tr>'
